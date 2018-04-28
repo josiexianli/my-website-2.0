7762dc6314c2565291c38e0b01bdf6a215b555c2
od006 <od006@od006deMBP.local>
2018-04-27 17:36:12 -0700

    CHANGE OF ICON

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home/page_root', { title: 'Express' });
+  res.render('home/page_root', { title: 'Xian Li' });
 });
 
 module.exports = router;
