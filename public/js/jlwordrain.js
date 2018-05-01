92f7485f36f5687e8d142c137d7d2f9aed548069
Yuan Yao <yuanyao@Yuans-MacBook-Pro-7.local>
2018-04-30 18:58:53 -0700

    SAVE FOR MERGE COLOR

--- public/js/jlwordrain.js
+++ public/js/jlwordrain.js
@@ -25,7 +25,7 @@ function draw() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
     ctx.fillRect(0, 0, c.width, c.height);
 
-    ctx.fillStyle = "#FF9000"; //green text
+    ctx.fillStyle = "#e67e22"; //green text
     ctx.font = font_size + "px arial";
     //looping over drops
     for (var i = 0; i < drops.length; i++) {
